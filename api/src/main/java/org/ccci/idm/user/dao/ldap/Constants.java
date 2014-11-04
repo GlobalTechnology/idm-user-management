db1a60ffbf290bdf44e1e3a3a366da7bfa836d31
Lee Braddock <lee.braddock@cru.org>
2014-11-04 16:18:50 -0500

    Set LDAP_ATTR_THEKEY_GUID name.

--- api/src/main/java/org/ccci/idm/user/dao/ldap/Constants.java
+++ api/src/main/java/org/ccci/idm/user/dao/ldap/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String LDAP_ATTR_EMAIL = "cn";
     public static final String LDAP_ATTR_USERID = "uid";
     public static final String LDAP_ATTR_GUID = "extensionAttribute1";
-    public static final String LDAP_ATTR_THEKEY_GUID = "extensionAttribute1";
+    public static final String LDAP_ATTR_THEKEY_GUID = "theKeyGuid";
     public static final String LDAP_ATTR_PASSWORD = "userPassword";
     public static final String LDAP_ATTR_FIRSTNAME = "givenName";
     public static final String LDAP_ATTR_LASTNAME = "sn";
