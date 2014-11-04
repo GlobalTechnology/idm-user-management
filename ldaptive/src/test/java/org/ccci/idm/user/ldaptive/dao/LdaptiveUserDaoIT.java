3f12113a30796d525295a9d3e1cf153671d33213
Lee Braddock <lee.braddock@cru.org>
2014-11-04 16:20:11 -0500

    Add create, find, and update ldaptive user dao test methods.

--- ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/LdaptiveUserDaoIT.java
+++ ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/LdaptiveUserDaoIT.java
@@ -2,7 +2,9 @@ package org.ccci.idm.user.ldaptive.dao;
 
 import static org.junit.Assume.assumeNotNull;
 
+import com.google.common.collect.Sets;
 import org.ccci.idm.user.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
 import java.security.SecureRandom;
+import java.util.Collection;
 import java.util.Random;
 import java.util.UUID;
 
@@ -49,12 +52,64 @@ public class LdaptiveUserDaoIT {
     }
 
     @Test
+    public void testFindUser() throws Exception {
+        assumeConfigured();
+
+        final User user = getUser();
+
+        this.dao.save(user);
+
+        final User foundUser = this.dao.findByEmail(user.getEmail());
+
+        Assert.assertTrue(user.equals(foundUser));
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        assumeConfigured();
+
+        User user = getUser();
+
+        this.dao.save(user);
+
+        user.setFirstName(user.getFirstName() + "modified");
+
+        this.dao.update(user, User.Attr.NAME);
+
+        final User foundUser = this.dao.findByEmail(user.getEmail());
+
+        Assert.assertTrue(user.equals(foundUser));
+    }
+
+    @Test
     public void testCreateStaffUser() throws Exception {
         assumeConfigured();
 
         final User user = getStaffUser();
 
         this.dao.save(user);
+    }
+
+    @Test
+    public void testUpdateStaffUser() throws Exception {
+        assumeConfigured();
+
+        final User user = getStaffUser();
+
+        this.dao.save(user);
+
+        User foundUser = this.dao.findByEmail(user.getEmail());
+
+        Assert.assertTrue(user.equals(foundUser));
+
+        user.setCity(user.getCity() + "modified");
+        user.setEmployeeId(user.getEmployeeId() + "modified");
+
+        this.dao.update(user, User.Attr.LOCATION, User.Attr.CRU_PERSON);
+
+        foundUser = this.dao.findByEmail(user.getEmail());
+
+        Assert.assertTrue(user.equals(foundUser));
     }
 
     private User getUser()
@@ -79,6 +134,8 @@ public class LdaptiveUserDaoIT {
         user.setCity("Orlando");
         user.setState("FL");
         user.setPostal("32832");
+        Collection<String> collection = Sets.newHashSet("smtp:test.user@cru.org", "smtp:test.user@ccci.org");
+        user.setCruProxyAddresses(collection);
 
         return user;
     }
