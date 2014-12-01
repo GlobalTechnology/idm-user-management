0bf99a56753cf8fd7517d2dc24c96dc4998c963c
Daniel Frett <daniel.frett@ccci.org>
2014-12-01 15:43:22 -0500

    add module encompassing inspektr auditing support

---
+++ inspektr/src/main/java/org/ccci/idm/user/inspektr/spi/UserAuditResourceResolver.java
@@ -0,0 +1,15 @@
+package org.ccci.idm.user.inspektr.spi;
+
+import com.github.inspektr.audit.spi.support.AbstractAuditResourceResolver;
+import org.ccci.idm.user.User;
+
+public class UserAuditResourceResolver extends AbstractAuditResourceResolver {
+    @Override
+    protected String[] createResource(final Object[] args) {
+        if (args.length >= 1 && args[0] instanceof User) {
+            final User user = (User) args[0];
+            return new String[]{"{guid=" + user.getGuid() + ",email=" + user.getEmail() + "}"};
+        }
+        return null;
+    }
+}
