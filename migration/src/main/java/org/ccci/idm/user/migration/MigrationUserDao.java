a0710d847de1ab19e431a5466a546a84309c4216
Daniel Frett <daniel.frett@ccci.org>
2015-01-09 11:02:16 -0500

    first pass at code for moving a Key account to the new ou

--- migration/src/main/java/org/ccci/idm/user/migration/MigrationUserDao.java
+++ migration/src/main/java/org/ccci/idm/user/migration/MigrationUserDao.java
@@ -1,5 +1,8 @@
 package org.ccci.idm.user.migration;
 
+import org.ccci.idm.user.User;
 import org.ccci.idm.user.dao.UserDao;
 
-public interface MigrationUserDao extends UserDao {}
+public interface MigrationUserDao extends UserDao {
+    void moveLegacyKeyUser(User user);
+}
