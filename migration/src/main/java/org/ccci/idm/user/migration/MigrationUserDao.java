4a95ffba07468ff75fda3b9dbe944948e49fbc15
Daniel Frett <daniel.frett@ccci.org>
2015-01-09 14:07:26 -0500

    implement a getAllLegacyUsers method

--- migration/src/main/java/org/ccci/idm/user/migration/MigrationUserDao.java
+++ migration/src/main/java/org/ccci/idm/user/migration/MigrationUserDao.java
@@ -3,6 +3,8 @@ package org.ccci.idm.user.migration;
 import org.ccci.idm.user.User;
 import org.ccci.idm.user.dao.UserDao;
 
+import java.util.List;
+
 public interface MigrationUserDao extends UserDao {
     void moveLegacyKeyUser(User user);
 
@@ -11,4 +13,6 @@ public interface MigrationUserDao extends UserDao {
     User findLegacyKeyByTheKeyGuid(String guid, boolean includeDeactivated);
 
     User findLegacyKeyByEmail(String email, boolean includeDeactivated);
+
+    List<User> findAllLegacyKeyUsers(boolean includeDeactivated);
 }
