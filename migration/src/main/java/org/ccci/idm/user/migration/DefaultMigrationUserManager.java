3da6b9da7ad108e64c7c8c63069a4ec6443900ae
Daniel Frett <daniel.frett@ccci.org>
2015-01-09 11:12:23 -0500

    add moveLegacyKeyUser method to UserManager
    I may need to change the migration userDao object once I get to spring config

--- migration/src/main/java/org/ccci/idm/user/migration/DefaultMigrationUserManager.java
+++ migration/src/main/java/org/ccci/idm/user/migration/DefaultMigrationUserManager.java
@@ -1,7 +1,22 @@
 package org.ccci.idm.user.migration;
 
 import org.ccci.idm.user.DefaultUserManager;
+import org.ccci.idm.user.User;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 
 public class DefaultMigrationUserManager extends DefaultUserManager implements MigrationUserManager {
+    @Inject
+    @NotNull
+    protected MigrationUserDao migrationUserDao;
 
+    public void setMigrationUserDao(final MigrationUserDao migrationUserDao) {
+        this.migrationUserDao = migrationUserDao;
+    }
+
+    @Override
+    public void moveLegacyKeyUser(final User user) {
+        this.migrationUserDao.moveLegacyKeyUser(user);
+    }
 }
--- migration/src/main/java/org/ccci/idm/user/migration/MigrationUserManager.java
+++ migration/src/main/java/org/ccci/idm/user/migration/MigrationUserManager.java
@@ -1,5 +1,8 @@
 package org.ccci.idm.user.migration;
 
+import org.ccci.idm.user.User;
 import org.ccci.idm.user.UserManager;
 
-public interface MigrationUserManager extends UserManager {}
+public interface MigrationUserManager extends UserManager {
+    void moveLegacyKeyUser(User user);
+}
