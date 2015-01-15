0cd178c54a9559085455e3fbb9f85c2ce3c98427
Daniel Frett <daniel.frett@ccci.org>
2015-01-15 14:39:47 -0500

    completely disable email validation, even whitespace checks

--- api/src/main/java/org/ccci/idm/user/DefaultUserManager.java
+++ api/src/main/java/org/ccci/idm/user/DefaultUserManager.java
@@ -1,12 +1,9 @@
 package org.ccci.idm.user;
 
 import com.github.inspektr.audit.annotation.Audit;
-import com.google.common.base.CharMatcher;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.ccci.idm.user.dao.ExceededMaximumAllowedResultsException;
 import org.ccci.idm.user.dao.UserDao;
 import org.ccci.idm.user.exception.EmailAlreadyExistsException;
-import org.ccci.idm.user.exception.InvalidEmailUserException;
 import org.ccci.idm.user.exception.RelayGuidAlreadyExistsException;
 import org.ccci.idm.user.exception.TheKeyGuidAlreadyExistsException;
 import org.ccci.idm.user.exception.UserException;
@@ -277,8 +274,8 @@ public class DefaultUserManager implements UserManager {
 
     protected void validateUser(final User user) throws UserException {
         // throw an error if we don't have a valid email
-        if (CharMatcher.WHITESPACE.matchesAnyOf(user.getEmail())) {
-            throw new InvalidEmailUserException("Invalid email specified for user");
-        }
+//        if (CharMatcher.WHITESPACE.matchesAnyOf(user.getEmail())) {
+//            throw new InvalidEmailUserException("Invalid email specified for user");
+//        }
     }
 }
