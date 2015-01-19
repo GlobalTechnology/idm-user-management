c646fd123c50d490004ffb6caf0d68c0b1446026
Daniel Frett <daniel.frett@ccci.org>
2015-01-19 16:16:31 -0500

    centralize some common testing utilities

---
+++ api/src/test/java/org/ccci/idm/user/TestUtil.java
@@ -0,0 +1,33 @@
+package org.ccci.idm.user;
+
+import javax.annotation.Nonnull;
+import java.security.SecureRandom;
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
+
+public class TestUtil {
+    private static final Random RAND = new SecureRandom();
+
+    @Nonnull
+    public static String guid() {
+        return UUID.randomUUID().toString().toUpperCase(Locale.US);
+    }
+
+    @Nonnull
+    public static User newUser() {
+        final User user = new User();
+        user.setEmail(randomEmail());
+        user.setGuid(guid());
+        user.setRelayGuid(guid());
+        user.setPassword("testPassword");
+        user.setFirstName("Test");
+        user.setLastName("User");
+        return user;
+    }
+
+    @Nonnull
+    public static String randomEmail() {
+        return "test.user." + RAND.nextInt(Integer.MAX_VALUE) + "@example.com";
+    }
+}
--- api/src/test/java/org/ccci/idm/user/UserTest.java
+++ api/src/test/java/org/ccci/idm/user/UserTest.java
@@ -1,5 +1,7 @@
 package org.ccci.idm.user;
 
+import static org.ccci.idm.user.TestUtil.guid;
+import static org.ccci.idm.user.TestUtil.randomEmail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
@@ -9,16 +11,10 @@ import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 
 import java.security.SecureRandom;
-import java.util.Locale;
 import java.util.Random;
-import java.util.UUID;
 
 public class UserTest {
     private Random RAND = new SecureRandom();
-
-    private static String guid() {
-        return UUID.randomUUID().toString().toUpperCase(Locale.US);
-    }
 
     @Test
     public void testGuidFallbacks() throws Exception {
@@ -96,7 +92,7 @@ public class UserTest {
             user.setTheKeyGuid(guid());
 
             // use random guid as random strings
-            user.setEmail(guid());
+            user.setEmail(randomEmail());
             user.setPassword(guid());
             user.setFirstName(guid());
             user.setLastName(guid());
@@ -144,15 +140,15 @@ public class UserTest {
             // test behavior when no verified flag is specified
             user.setEmailVerified(true);
             assertTrue(user.isEmailVerified());
-            user.setEmail("user1@example.com");
+            user.setEmail(randomEmail());
             assertFalse(user.isEmailVerified());
 
             // test behavior when verified flag is specified
             user.setEmailVerified(true);
             assertTrue(user.isEmailVerified());
-            user.setEmail("user2@example.com", false);
+            user.setEmail(randomEmail(), false);
             assertFalse(user.isEmailVerified());
-            user.setEmail("user3@example.com", true);
+            user.setEmail(randomEmail(), true);
             assertTrue(user.isEmailVerified());
         }
     }
