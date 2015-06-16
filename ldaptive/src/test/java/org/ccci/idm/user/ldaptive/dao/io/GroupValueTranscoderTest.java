c5773db314eef7079ff735b32ca5ad3cca303a93
Daniel Frett <daniel.frett@ccci.org>
2015-06-16 12:00:35 -0400

    make GroupValueTranscoderTest parameterized

--- ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/io/GroupValueTranscoderTest.java
+++ ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/io/GroupValueTranscoderTest.java
@@ -2,37 +2,37 @@ package org.ccci.idm.user.ldaptive.dao.io;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeNotNull;
 
 import org.ccci.idm.user.Group;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Collection;
 
-public class GroupValueTranscoderTest
-{
-    private static GroupValueTranscoder groupDnResolver;
-    private static final String baseDn = "ou=groups,ou=idm,dc=cru,dc=org";
+@RunWith(Parameterized.class)
+public class GroupValueTranscoderTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{{null}, {""}, {"ou=groups,ou=idm,dc=cru,dc=org"}});
+    }
 
-    private void assumeConfigured() throws Exception {
-        assumeNotNull(groupDnResolver);
+    @Nonnull
+    private final GroupValueTranscoder groupDnResolver;
+
+    public GroupValueTranscoderTest(@Nullable final String baseDn) {
+        groupDnResolver = new GroupValueTranscoder();
+        groupDnResolver.setBaseDn(baseDn);
     }
 
     private String name = "Mail";
     private String[] path = new String[] {"GoogleApps", "Cru", "Cru"};
 
-    @BeforeClass
-    public static void beforeClass()
-    {
-        groupDnResolver = new GroupValueTranscoder();
-        groupDnResolver.setBaseDn(baseDn);
-    }
-
     @Test
     public void testGroupDnResolver() throws Exception {
-        assumeConfigured();
-
         String groupDn = "cn=" + name + ",ou=Cru,ou=Cru,ou=GoogleApps," + groupDnResolver.getBaseDn();
 
         Group group = groupDnResolver.decodeStringValue(groupDn);
@@ -45,8 +45,6 @@ public class GroupValueTranscoderTest
 
     @Test
     public void testGroupDnResolverCaseInsensitiveDn() throws Exception {
-        assumeConfigured();
-
         String groupDn = "CN=" + name + ",OU=Cru,ou=Cru,OU=GoogleApps," +
                 groupDnResolver.getBaseDn().toUpperCase();
 
@@ -60,8 +58,6 @@ public class GroupValueTranscoderTest
 
     @Test
     public void testGroupNotMutated() throws Exception {
-        assumeConfigured();
-
         String groupDn = "cn=" + name + ",ou=Cru,ou=Cru,ou=GoogleApps," + groupDnResolver.getBaseDn();
 
         Group group = groupDnResolver.decodeStringValue(groupDn);
