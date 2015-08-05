bbd540ccbfe8162be85b9c2e8246afa2480c85b9
Daniel Frett <daniel.frett@ccci.org>
2015-08-05 11:55:05 -0400

    create custom resource resolver for UpdateUser audits
    The custom resolver will include the attributes being updated in the Audit Resource message

--- inspektr/src/main/java/org/ccci/idm/user/inspektr/spi/UserAuditResourceResolver.java
+++ inspektr/src/main/java/org/ccci/idm/user/inspektr/spi/UserAuditResourceResolver.java
@@ -1,15 +1,25 @@
 package org.ccci.idm.user.inspektr.spi;
 
 import com.github.inspektr.audit.spi.support.AbstractAuditResourceResolver;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
 import org.ccci.idm.user.User;
+
+import javax.annotation.Nonnull;
 
 public class UserAuditResourceResolver extends AbstractAuditResourceResolver {
     @Override
     protected String[] createResource(final Object[] args) {
         if (args.length >= 1 && args[0] instanceof User) {
             final User user = (User) args[0];
-            return new String[]{"{guid=" + user.getGuid() + ",email=" + user.getEmail() + "}"};
+            return new String[]{userToString(user).toString()};
         }
+
         return null;
+    }
+
+    @Nonnull
+    protected ToStringHelper userToString(@Nonnull final User user) {
+        return MoreObjects.toStringHelper(user).add("guid", user.getGuid()).add("email", user.getEmail());
     }
 }
