4f8ebf76919929d4f35bd53bf233e8541bde805d
Daniel Frett <daniel.frett@ccci.org>
2016-05-03 19:55:39 -0400

    improve the current group decoder tests

--- ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/io/GroupValueTranscoderTest.java
+++ ldaptive/src/test/java/org/ccci/idm/user/ldaptive/dao/io/GroupValueTranscoderTest.java
@@ -1,6 +1,8 @@
 package org.ccci.idm.user.ldaptive.dao.io;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -23,6 +25,10 @@ public class GroupValueTranscoderTest {
         return Arrays.asList(new Object[][]{{null}, {""}, {"ou=groups,ou=idm,dc=cru,dc=org"}});
     }
 
+    private static String NAME = "Mail";
+    private static String[] PATH = new String[]{"GoogleApps", "Cru", "Cru"};
+    private Group GROUP = new Group(PATH, NAME);
+
     private final String groupSuffix;
     @Nonnull
     private final GroupValueTranscoder groupDnResolver;
@@ -33,41 +39,47 @@ public class GroupValueTranscoderTest {
         groupDnResolver.setBaseDn(baseDn);
     }
 
-    private String name = "Mail";
-    private String[] path = new String[] {"GoogleApps", "Cru", "Cru"};
-
     @Test
-    public void testGroupDnResolver() throws Exception {
-        final String groupDn = "cn=" + name + ",ou=Cru,ou=Cru,ou=GoogleApps" + groupSuffix;
+    public void verifyDecodeStringValue() throws Exception {
+        final String groupDn = "cn=" + NAME + ",ou=Cru,ou=Cru,ou=GoogleApps" + groupSuffix;
 
         Group group = groupDnResolver.decodeStringValue(groupDn);
 
-        assertEquals(name, group.getName());
-        assertEquals(Arrays.toString(path), Arrays.toString(group.getPath()));
+        assertEquals(NAME, group.getName());
+        assertThat(group, is(GROUP));
+        assertEquals(Arrays.toString(PATH), Arrays.toString(group.getPath()));
 
         assertEquals(groupDn, groupDnResolver.encodeStringValue(group));
     }
 
     @Test
-    public void testGroupDnResolverCaseInsensitiveDn() throws Exception {
-        final String groupDn = "CN=" + name + ",OU=Cru,ou=Cru,OU=GoogleApps" + groupSuffix.toUpperCase();
+    public void verifyDecodeStringValueCaseInsensitiveDn() throws Exception {
+        final String groupDn = "CN=" + NAME + ",OU=Cru,ou=Cru,OU=GoogleApps" + groupSuffix.toUpperCase();
 
         Group group = groupDnResolver.decodeStringValue(groupDn);
 
-        assertEquals(name, group.getName());
-        assertEquals(Arrays.toString(path), Arrays.toString(group.getPath()));
+        assertThat(group, is(GROUP));
+        assertEquals(NAME, group.getName());
+        assertEquals(Arrays.toString(PATH), Arrays.toString(group.getPath()));
 
         assertTrue(groupDn.equalsIgnoreCase(groupDnResolver.encodeStringValue(group)));
     }
 
     @Test
     public void testGroupNotMutated() throws Exception {
-        final String groupDn = "cn=" + name + ",ou=Cru,ou=Cru,ou=GoogleApps" + groupSuffix;
+        final String groupDn = "cn=" + NAME + ",ou=Cru,ou=Cru,ou=GoogleApps" + groupSuffix;
 
         Group group = groupDnResolver.decodeStringValue(groupDn);
 
+        assertThat(group, is(GROUP));
         assertEquals(groupDn, groupDnResolver.encodeStringValue(group));
-        assertEquals(groupDn, groupDnResolver.encodeStringValue(group));
+    }
+
+    @Test
+    public void verifyEncodeStringValue() throws Exception {
+        final String groupDn = "CN=" + NAME + ",OU=Cru,ou=Cru,OU=GoogleApps" + groupSuffix.toUpperCase();
+
+        assertTrue(groupDn.equalsIgnoreCase(groupDnResolver.encodeStringValue(GROUP)));
     }
 
     @Test
