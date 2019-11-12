606d14855e58cf8ad75b30be35c0b76b08e4bdda
Daniel Frett <daniel.frett@ccci.org>
2019-11-12 11:57:34 -0500

    create a FallbackDao Listener for the OktaUserDao
    this listener will allow the OktaUserDao to fallback on LDAP for several attributes that aren't being ported to Okta

---
+++ okta/src/main/java/org/ccci/idm/user/okta/dao/FallbackDaoOktaUserDaoListener.kt
@@ -0,0 +1,35 @@
+package org.ccci.idm.user.okta.dao
+
+import org.ccci.idm.user.User
+import org.ccci.idm.user.dao.UserDao
+
+private val UPDATABLE_ATTRS = setOf(User.Attr.MFA_SECRET, User.Attr.MFA_INTRUDER_DETECTION, User.Attr.SELFSERVICEKEYS)
+
+class FallbackDaoOktaUserDaoListener(
+    private val dao: UserDao
+) : OktaUserDao.Listener {
+    override fun onUserLoaded(user: User) {
+        dao.findByTheKeyGuid(user.theKeyGuid, true)?.run {
+            // MFA attributes
+            user.isMfaBypassed = isMfaBypassed
+            user.mfaEncryptedSecret = mfaEncryptedSecret
+            user.isMfaIntruderLocked = isMfaIntruderLocked
+            user.mfaIntruderAttempts = mfaIntruderAttempts
+            user.mfaIntruderResetTime = mfaIntruderResetTime
+
+            // self-service keys
+            user.signupKey = signupKey
+            user.proposedEmail = proposedEmail
+            user.changeEmailKey = changeEmailKey
+            user.resetPasswordKey = resetPasswordKey
+        }
+    }
+
+    override fun onUserUpdated(user: User, vararg attrs: User.Attr) {
+        val filteredAttrs = attrs.filter { UPDATABLE_ATTRS.contains(it) }
+        if (filteredAttrs.isEmpty()) return
+
+        val original = dao.findByTheKeyGuid(user.theKeyGuid, true)
+        dao.update(original, user, *filteredAttrs.toTypedArray())
+    }
+}
